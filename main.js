ba873e1bb791fc8051f1adee207f1344dfbee0cc
francineunakazm <74522592+francineunakazm@users.noreply.github.com>
2024-01-19 02:30:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["an Artist" , "a Musician" , "an aspiring Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
